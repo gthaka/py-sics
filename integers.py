474268c3f2fe0a30440c1fdbb2c209de6d0690a6
Anthony G <agithaka@live.com>
2023-09-21 10:23:34 +0300

    integers

---
+++ integers.py
@@ -0,0 +1,67 @@
+num = 3
+print(type(num))
+
+num = 3.14
+print(type(num))
+
+print("arithmetic")
+# Arithmetic Operators:
+# Addition:
+print(3 + 2)
+# Subtraction
+print(3 - 2)
+# Multiplication
+print(3 * 2)
+# Divisio
+print(3 / 2)
+# Floor Division - drops decimal
+print(3 // 2)
+# Exponent
+print(3**2)
+# Modulus
+print(3 % 2)
+print(4 % 2)
+print(5 % 2)
+print(2 % 2)
+
+# order of operations
+print(3 * 2 + 1)
+print(3 * (2 + 1))
+
+# increment variable
+num = 1
+num = num + 1
+print(num)
+
+num += 1
+print(num)
+
+num *= 10
+print(num)
+
+# absolute value
+print(abs(-3))
+# rounding
+print(round(3.75))
+print(round(3.75, 1))
+
+# comparison operators
+# equal
+print(3 == 2)
+# not equal
+print(3 != 2)
+# greater than
+print(3 > 2)
+# less than
+print(3 < 2)
+# greater or equal
+print(3 >= 2)
+# less or equal
+print(3 <= 2)
+
+#
+num_1 = "200"
+num_2 = "400"
+print(num_1 + num_2)
+# casting
+print(int(num_1) + int(num_2))
